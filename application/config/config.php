abafd8fad13b7c5ae223b1321d121b2acf33f812
Faruq Maulana <88839109+faruqmaulana@users.noreply.github.com>
2024-08-20 02:17:13 +0700

    update config

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://app.gooproper.id/';
+$config['base_url'] = 'https://ci-app.gooproper.com/';
 
 /*
 |--------------------------------------------------------------------------
@@ -387,7 +387,8 @@ $config['sess_driver'] = 'files';
 $config['sess_cookie_name'] = 'ci_session';
 $config['sess_samesite'] = 'Lax';
 $config['sess_expiration'] = 7200;
-$config['sess_save_path'] = NULL;
+// $config['sess_save_path'] = NULL;
+$config['sess_save_path'] = '/var/www/html/sessions';
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
 $config['sess_regenerate_destroy'] = FALSE;
